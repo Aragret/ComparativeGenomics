ddde263b209b38c4134ea19a603230831910cae8
Alina Mikhailova <mihailovaalina777@yandex.ru>
2018-12-27 23:28:00 +0200

    files for alignment

--- Head/2Scripts/TablesForAlign.R
+++ Head/2Scripts/TablesForAlign.R
@@ -3,7 +3,7 @@ library(seqinr)
 library(gdata)
 
 CHOR = read.table('../../Body/2Derived/MitGenomics.txt', header=TRUE, sep='\t')
-CodonTable = unzip('../../Body/2Derived/AllGenesCodons.zip')
+CodonTable = unzip('../../Body/2Derived/AllGenesCodonUsageNoOverlap.txt.zip')
 CodonTable = read.table(CodonTable, header=TRUE, sep='\t')
 GenLength = read.xls('../../Body/1Raw/GenerationLengthForMammals.xlsx')
 
@@ -12,7 +12,6 @@ GenLength = GenLength[,c(14,16)]
 
 ### all 13 proteins
 
-CodonTable = CodonTable[CodonTable$Quality == 1,]
 for(i in unique(CodonTable$Species)){
   if(nrow(CodonTable[CodonTable$Species == i,]) != 13){
     CodonTable = CodonTable[CodonTable$Species != i,]
@@ -24,28 +23,23 @@ genes = as.character(unique(CodonTable$Gene))
 
 ### ecology
 
-CHOR = CHOR[, c('Species', 'TAXON', 'ECO.Female.maturity..days.', 'ECO.Maximum.longevity..yrs.',
+CHOR = CHOR[, c('Species', 'ECO.Female.maturity..days.', 'ECO.Maximum.longevity..yrs.',
                 'ECO.Body.mass..g.')]
 CHOR = merge(CHOR, GenLength, by='Species', all.x=TRUE)
 data = merge(CodonTable, CHOR, by='Species')
 
-Final <- data[rowSums(is.na(data[, c(10,11,12,13)])) != 4,]
+Final <- data[rowSums(is.na(data[, c(79:82)])) != 4,]
 
 length(unique(Final$Species)) # 1300
 
-Actinopterygii = Final[Final$TAXON == 'Actinopterygii',] # 217 sp.
-Reptilia = Final[Final$TAXON == 'Reptilia',] # 93
-Amphibia = Final[Final$TAXON == 'Amphibia',] # 32
-Mammalia = Final[Final$TAXON == 'Mammalia',] # 731
-Aves = Final[Final$TAXON == 'Aves',] # 180
-
 #### write to fasta
+dir.create('../../Body/2Derived/ForAlign')
 
 for(i in unique(Final$Gene)){
-  for(j in unique(Final$TAXON)){
+  for(j in unique(Final$Class)){
     GeneTable = Final[Final$Gene == i,]
-    GeneTaxonTable = GeneTable[GeneTable$TAXON == j,]
-    write.fasta(as.list(GeneTaxonTable$ModifiedAmino), GeneTaxonTable$Species, sprintf('../../Body/2Derived/ForAlign/%s_%s.fna', i, j),
+    GeneTaxonTable = GeneTable[GeneTable$Class == j,]
+    write.fasta(as.list(GeneTaxonTable$AminoNoOverlap), GeneTaxonTable$Species, sprintf('../../Body/2Derived/ForAlign/%s_%s.fna', i, j),
                 open = 'w')
   }
 }
