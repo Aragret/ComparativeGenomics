4508cb937b6c190293b0cd83d014b2a4aeddfa9e
Alina Mikhailova <mihailovaalina777@yandex.ru>
2019-06-29 17:40:16 +0200

    added taxons in corr

--- Head/2Scripts/RepeatsVsGenomeDloopLength.R
+++ Head/2Scripts/RepeatsVsGenomeDloopLength.R
@@ -3,6 +3,7 @@
 rm(list=ls(all=TRUE))  # remove everything from R memory (old variables, datasets...) 
 
 CHOR = read.table('../../Body/2Derived/MitGenomics.txt', header=TRUE, sep='\t')
+CHOR = CHOR[CHOR$TAXON != 'AncientFish',]
 
 
 cor.test(CHOR$GenomeLength, CHOR$REP.LengthOfTandemRepeats, method = 'spearman')
@@ -11,7 +12,17 @@ cor.test(CHOR$GenomeLength, CHOR$REP.SymmRepLength, method = 'spearman')
 cor.test(CHOR$GenomeLength, CHOR$REP.ComplRepLength, method = 'spearman')
 cor.test(CHOR$GenomeLength, CHOR$REP.InvRepLength, method = 'spearman')
 
+taxon = unique(CHOR$TAXON)
 
+for (i in taxon) {
+  tempData = CHOR[CHOR$TAXON == i,]
+  print(i)
+  print(c(cor.test(tempData$GenomeLength, tempData$REP.LengthOfTandemRepeats, method = 'spearman'),
+        cor.test(tempData$GenomeLength, tempData$REP.DirRepLength, method = 'spearman'),
+        cor.test(tempData$GenomeLength, tempData$REP.SymmRepLength, method = 'spearman'),
+        cor.test(tempData$GenomeLength, tempData$REP.ComplRepLength, method = 'spearman'),
+        cor.test(tempData$GenomeLength, tempData$REP.InvRepLength, method = 'spearman')))
+}
 
 a = summary(lm(CHOR$GenomeLength ~ CHOR$REP.LengthOfTandemRepeats + CHOR$REP.DirRepLength + CHOR$REP.SymmRepLength +
        CHOR$REP.ComplRepLength + CHOR$REP.InvRepLength)); a
