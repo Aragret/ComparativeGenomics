7d1b227ca50f4b8f1b17b3e24c168d922c23e35c
Alina Mikhailova <mihailovaalina777@yandex.ru>
2019-08-30 11:46:56 +0900

    Simple contrasts for branch lengths

--- Head/2Scripts/BranchLengthVsGenerationLength.R
+++ Head/2Scripts/BranchLengthVsGenerationLength.R
@@ -25,3 +25,52 @@ externalBranchesGl = merge(externalBranches, GenLength, by='Species')
 cor.test(externalBranchesGl$GenerationLength_d, externalBranchesGl$BranchLength, method = 'spearman')
 # rho -0.2042569
 # pvalue 4.364e-08
+
+
+############## how to get sisters species
+
+library(geiger)
+
+max_node_number = max(tree$edge)
+min_node_number = length(tree$tip.label) + 1
+
+one_line = c()
+for (i in min_node_number:max_node_number){
+  descendants = tips(tree, i)
+  if (length(descendants) == 2){
+    one_line = rbind(one_line, c(i, descendants))
+  }
+}
+
+sisters = as.data.frame(one_line)
+names(sisters) = c('NodeNumber', 'Species1', 'Species2')
+
+
+###########
+
+sistersGL = merge(sisters, GenLength, by.x='Species1', by.y = "Species")
+sistersGLfull = merge(sistersGL, GenLength, by.x='Species2', by.y = "Species")
+
+branchLengthsVec = c(NULL)
+for(i in 1:nrow(sistersGLfull)){
+  # i = 1
+  diff = sistersGLfull[i, 'GenerationLength_d.x'] - sistersGLfull[i, 'GenerationLength_d.y']
+  if(diff > 0){
+    longsp = as.character(sistersGLfull[i, 'Species1'])
+    shortsp = as.character(sistersGLfull[i, 'Species2'])
+  }
+  if(diff < 0){
+    longsp = as.character(sistersGLfull[i, 'Species2'])
+    shortsp = as.character(sistersGLfull[i, 'Species1'])
+  }
+  branchLongsp = externalBranchesGl[externalBranchesGl$Species == longsp, 'BranchLength']
+  branchShortsp = externalBranchesGl[externalBranchesGl$Species == shortsp, 'BranchLength']
+  a = branchLongsp - branchShortsp
+  branchLengthsVec = c(branchLengthsVec, a)
+}
+
+pdf('../../Body/4Figures/BranchLengthDiff.pdf')
+summary(branchLengthsVec)
+hist(branchLengthsVec, breaks = 50)
+
+dev.off()
