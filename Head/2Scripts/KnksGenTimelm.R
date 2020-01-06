2e5cfdb7cbac2f231d56cad1c8df67b72fa569cd
Alina Mikhailova <mihailovaalina777@yandex.ru>
2020-01-06 23:02:47 +0900

    Oikos data

--- Head/2Scripts/KnksGenTimelm.R
+++ Head/2Scripts/KnksGenTimelm.R
@@ -5,3 +5,9 @@ data = merge(kaks, CHOR[, c('Species', 'REP.NumberOfTandemRepeats')], by.x = 'SP
              by.y = 'Species')
 
 summary(lm(data$lnDN_DS ~ data$lnBM_g + data$REP.NumberOfTandemRepeats))
+
+# Coefficients:
+#   Estimate Std. Error t value Pr(>|t|)    
+# (Intercept)                    -3.623693   0.100810 -35.946  < 2e-16 ***
+#   data$lnBM_g                     0.045442   0.009604   4.732 4.23e-06 ***
+#   data$REP.NumberOfTandemRepeats -0.061748   0.022294  -2.770  0.00614 ** 
